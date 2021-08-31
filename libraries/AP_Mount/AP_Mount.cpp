6d1d27b24451d213eb784f4063d38a56d3400d76
Abner0907 <wen.huang@codevdynamics.com>
2021-08-31 15:43:06 +0800

    AP_Mount:  Fix the gimbal which index need increase.
    Signed-off-by: Abner0907 <wen.huang@codevdynamics.com>

--- libraries/AP_Mount/AP_Mount.cpp
+++ libraries/AP_Mount/AP_Mount.cpp
@@ -634,6 +634,7 @@ MAV_RESULT AP_Mount::handle_command_long(const mavlink_command_long_t &packet)
     case MAV_CMD_IMAGE_START_CAPTURE:
     case MAV_CMD_VIDEO_START_CAPTURE:
     case MAV_CMD_VIDEO_STOP_CAPTURE:
+    case MAV_CMD_SET_CAMERA_MODE:
         return handle_command_do_camera_control(packet);
     default:
         return MAV_RESULT_UNSUPPORTED;
--- libraries/AP_Mount/AP_Mount_Mavlink.cpp
+++ libraries/AP_Mount/AP_Mount_Mavlink.cpp
@@ -165,7 +165,7 @@ void AP_Mount_Mavlink::control_camera(const mavlink_command_long_t &packet)
                                   packet.param1,        // confirmation of zero means this is the first time this message has been sent
                                   packet.param2,
                                   packet.param3,
-                                  packet.param4,
+                                  _picture_count++,     //ToDo: Fix the e90x which index need increase.
                                   packet.param5,
                                   packet.param6,
                                   packet.param7);
