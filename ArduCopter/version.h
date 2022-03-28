b2b13854ceb3560045172158b4cd1b91c439d6a1
rainmaker64 <rainmaker64@argosdyne.com>
2022-03-28 14:14:35 +0900

    * version change: V2.0.0 -> V2.0.1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Aquila-I V2.0.0"
+#define THISFIRMWARE "Aquila-I V2.0.1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,7,FIRMWARE_VERSION_TYPE_OFFICIAL
